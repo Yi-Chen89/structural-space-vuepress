902dfcaed84da7a753a01d5e011129d13a60b970
Yi-Chen89 <58967830+Yi-Chen89@users.noreply.github.com>
2024-12-14 19:43:31 -0500

    Work on Compression Calculator
    - rename helper function E4TorsionalBucklingWithoutSlenderElementFcr() to E4_aTorsionalBucklingWithoutSlenderElementFcr()
    - rename helper function E4FlexuralTorsionalBucklingWithoutSlenderElementFcr() to E4_bFlexuralTorsionalBucklingWithoutSlenderElementFcr()
    - renmae helper function flexuralTorsionalElasticBucklingStressCalculator() to SinglySymmetricFlexuralTorsionalElasticBucklingStressCalculator()
    - create helper function E4_cFlexuralTorsionalBucklingWithoutSlenderElementFcr()
    - create helper function UnsymmetricFlexuralTorsionalElasticBucklingStressCalculator()
    - create helper function cubicEquationSolver()

--- docs/.vuepress/components/steel/utils/compression-calculators.js
+++ docs/.vuepress/components/steel/utils/compression-calculators.js
@@ -43,7 +43,7 @@ export function compressionCalculator(shapeData, shapeType, astmSpecProp, slende
       const [phi_3, Fcr_3, FcrHtml_3] = E3FlexuralBucklingWithoutSlenderElementFcr(Fy, E, rx, ry, Lcx, Lcy);
 
       // E4 Torsional Buckling
-      const [phi_4, Fcr_4, FcrHtml_4] = E4TorsionalBucklingWithoutSlenderElementFcr(Fy, E, G, Ix, Iy, J, Cw, Lcz);
+      const [phi_4, Fcr_4, FcrHtml_4] = E4_aTorsionalBucklingWithoutSlenderElementFcr(Fy, E, G, Ix, Iy, J, Cw, Lcz);
 
       if (flange === 'nonslender' && web === 'nonslender') {
         // E3 E4
@@ -89,7 +89,7 @@ export function compressionCalculator(shapeData, shapeType, astmSpecProp, slende
       const [phi_3, Fcr_3, FcrHtml_3] = E3FlexuralBucklingWithoutSlenderElementFcr(Fy, E, rx, ry, Lcx, Lcy);
 
       // E4 Flexural-Torsional Buckling
-      const [phi_4, Fcr_4, FcrHtml_4] = E4FlexuralTorsionalBucklingWithoutSlenderElementFcr(shapeType, Fy, E, G, A, rx, ry, J, Cw, ro, H, Lcx, Lcy, Lcz);
+      const [phi_4, Fcr_4, FcrHtml_4] = E4_bFlexuralTorsionalBucklingWithoutSlenderElementFcr(shapeType, Fy, E, G, A, rx, ry, J, Cw, ro, H, Lcx, Lcy, Lcz);
 
       if (flange === 'nonslender' && web === 'nonslender') {
         // E3 E4
@@ -127,7 +127,38 @@ export function compressionCalculator(shapeData, shapeType, astmSpecProp, slende
         result['Pn_4_7']['designValue'] = phi_4 * Pn_4_7;
         result['Pn_4_7']['html'] = FcrHtml_4 + AeHtml_4 + PnHtml_4_7;
       }
+    
+    } else if (['L'].includes(shapeType)) {
+      const { A, h, b, t, x, y, rx, ry, J, Cw, ro } = shapeData;
 
+      // E3 Flexural Buckling
+      const [phi_3, Fcr_3, FcrHtml_3] = E3FlexuralBucklingWithoutSlenderElementFcr(Fy, E, rx, ry, Lcx, Lcy);
+
+      // E4 Flexural-Torsional Buckling
+      const [phi_4, Fcr_4, FcrHtml_4] = E4_cFlexuralTorsionalBucklingWithoutSlenderElementFcr(Fy, E, G, A, t, x, y, rx, ry, J, Cw, ro, Lcx, Lcy, Lcz, lambdaw);
+
+      if (flange === 'nonslender' && web === 'nonslender') {
+        // E3 E4
+        // limit state: FB FTB
+        result['Pn_3']['isApplicable'] = true;
+        const [Pn_3, PnHtml_3] = capacityCalculator(Fcr_3, A, 'gross');
+        result['Pn_3']['phiValue'] = phi_3;
+        result['Pn_3']['nominalValue'] = Pn_3;
+        result['Pn_3']['designValue'] = phi_3 * Pn_3;
+        result['Pn_3']['html'] = FcrHtml_3 + PnHtml_3;
+        
+        result['Pn_4']['isApplicable'] = true;
+        const [Pn_4, PnHtml_4] = capacityCalculator(Fcr_4, A, 'gross');
+        result['Pn_4']['phiValue'] = phi_4;
+        result['Pn_4']['nominalValue'] = Pn_4;
+        result['Pn_4']['designValue'] = phi_4 * Pn_4;
+        result['Pn_4']['html'] = FcrHtml_4 + PnHtml_4;
+
+      } else if (flange === 'slender' || web === 'slender') {
+        // E7
+        // limit state: LB
+      }
+    
     } else if (['HSS Rect.', 'HSS Square'].includes(shapeType)) {
       const { A, h, b, tdes, rx, ry } = shapeData;
 
@@ -300,8 +331,8 @@ function E3FlexuralBucklingWithoutSlenderElementFcr(Fy, E, rx, ry, Lcx, Lcy) {
 
 // E4 Torsional and Flexural-Torsional Buckling of Single Angles and Members without Slender Elements
 
-// E4 Torsional Buckling of Members without Slender Elements
-function E4TorsionalBucklingWithoutSlenderElementFcr(Fy, E, G, Ix, Iy, J, Cw, Lcz) {
+// E4(a) Torsional Buckling of Members without Slender Elements
+function E4_aTorsionalBucklingWithoutSlenderElementFcr(Fy, E, G, Ix, Iy, J, Cw, Lcz) {
   let phi = 0.9;
   let Fcr = 0;
   let html = '';
@@ -322,8 +353,8 @@ function E4TorsionalBucklingWithoutSlenderElementFcr(Fy, E, G, Ix, Iy, J, Cw, Lc
   return [phi, Fcr, html];
 }
 
-// E4 Flexural-Torsional Buckling of Members without Slender Elements
-function E4FlexuralTorsionalBucklingWithoutSlenderElementFcr(shapeType, Fy, E, G, Ag, rx, ry, J, Cw, ro, H, Lcx, Lcy, Lcz) {
+// E4(b) Flexural-Torsional Buckling of Members without Slender Elements
+function E4_bFlexuralTorsionalBucklingWithoutSlenderElementFcr(shapeType, Fy, E, G, Ag, rx, ry, J, Cw, ro, H, Lcx, Lcy, Lcz) {
   let phi = 0.9;
   let Fcr = 0;
   let html = '';
@@ -339,7 +370,7 @@ function E4FlexuralTorsionalBucklingWithoutSlenderElementFcr(shapeType, Fy, E, G
         html += `<div>${Lcx_} must not be 0</div>`;
         return [phi, Fcr, html];
       } else {
-        [Fe, FeHtml] = flexuralTorsionalElasticBucklingStressCalculator('x', E, G, Ag, rx, J, Cw, ro, H, Lcx, Lcz);
+        [Fe, FeHtml] = SinglySymmetricFlexuralTorsionalElasticBucklingStressCalculator('x', E, G, Ag, rx, J, Cw, ro, H, Lcx, Lcz);
       }
 
     } else if (['WT', 'MT', 'ST'].includes(shapeType)) {
@@ -347,7 +378,7 @@ function E4FlexuralTorsionalBucklingWithoutSlenderElementFcr(shapeType, Fy, E, G
         html += `<div>${Lcy_} must not be 0</div>`;
         return [phi, Fcr, html];
       } else {
-        [Fe, FeHtml] = flexuralTorsionalElasticBucklingStressCalculator('y', E, G, Ag, ry, J, 0, ro, H, Lcy, Lcz);
+        [Fe, FeHtml] = SinglySymmetricFlexuralTorsionalElasticBucklingStressCalculator('y', E, G, Ag, ry, J, 0, ro, H, Lcy, Lcz);
       }
 
     }
@@ -361,8 +392,8 @@ function E4FlexuralTorsionalBucklingWithoutSlenderElementFcr(shapeType, Fy, E, G
   return [phi, Fcr, html];
 }
 
-// Flexural-Torsional Elastic Buckling Stress Calculator
-function flexuralTorsionalElasticBucklingStressCalculator(axisOfSym, E, G, Ag, r, J, Cw, ro, H, Lc, Lcz) {
+// Flexural-Torsional Elastic Buckling Stress Calculator for Singly Symmetric Members
+function SinglySymmetricFlexuralTorsionalElasticBucklingStressCalculator(axisOfSym, E, G, Ag, r, J, Cw, ro, H, Lc, Lcz) {
   let Fe = 0;
   let html = `<div>Flexural-torsional elastic buckling stress</div>
               <div class="indented-line">${ro_} = ${ro.toFixed(2)} in.&emsp;
@@ -396,6 +427,154 @@ function flexuralTorsionalElasticBucklingStressCalculator(axisOfSym, E, G, Ag, r
   html += `<div class="indented-line">${Fe_} = (${Fexy_} + ${Fez_}) / (2 ${H_}) (1 - &radic;(1 - 4 ${Fexy_} ${Fez_} ${H_} / (${Fexy_} + ${Fez_})<sup>2</sup>)) = ${Fe.toFixed(2)} ksi</div>`;
 
   return [Fe, html];
+}
+
+// E4(c) Flexural-Torsional Buckling of Members without Slender Elements
+function E4_cFlexuralTorsionalBucklingWithoutSlenderElementFcr(Fy, E, G, Ag, t, x, y, rx, ry, J, Cw, ro, Lcx, Lcy, Lcz, lambdaw) {
+  let phi = 0.9;
+  let Fcr = 0;
+  let html = '';
+
+  const calcTerm1 = 0.71 * Math.sqrt(E / Fy);
+  const calcTerm1_ = `0.71 &radic;(${E_} / ${Fy_})`;
+  if (lambdaw <= calcTerm1) {
+    html += `<div>For ${lambdaw_} &le; ${calcTerm1_}, flexural-torsional buckling does not apply</div>`;
+
+  } else {
+    if (Lcz === 0) {
+      html += `<div>For sections with continuous torsional bracing, flexural-torsional buckling does not apply</div>`;
+
+    } else {
+      let Fe = 0;
+      let FeHtml = '';
+
+      if (Lcx === 0 && Lcy === 0) {
+        html += `<div>${Lcx_} and ${Lcy_} must not be 0</div>`;
+        
+      } else if (Lcx === 0) {
+        html += `<div>${Lcx_} must not be 0</div>`;
+        
+      } else if (Lcy === 0) {
+        html += `<div>${Lcy_} must not be 0</div>`;
+        
+      } else {
+        [Fe, FeHtml] = UnsymmetricFlexuralTorsionalElasticBucklingStressCalculator(E, G, Ag, t, x, y, rx, ry, J, Cw, ro, Lcx, Lcy, Lcz);
+        html += FeHtml;
+
+        let FcrHtml = '';
+        [Fcr, FcrHtml] = criticalStressCalculator(Fy, Fe);
+        html += FcrHtml;
+      }
+    }
+  }
+
+  return [phi, Fcr, html];
+}
+
+// Flexural-Torsional Elastic Buckling Stress Calculator for Unsymmetric Members
+function UnsymmetricFlexuralTorsionalElasticBucklingStressCalculator(E, G, Ag, t, x, y, rx, ry, J, Cw, ro, Lcx, Lcy, Lcz) {
+  let Fe = 0;
+  let html = `<div>Flexural-torsional elastic buckling stress</div>
+              <div class="indented-line">${x_} = ${x.toFixed(3)} in.&emsp;
+                                         ${y_} = ${y.toFixed(3)} in.</div>
+              <div class="indented-line">${ro_} = ${ro.toFixed(2)} in.</div>`;
+
+  const Fex = Math.PI**2 * E / (Lcx / rx)**2;
+  html += `<div class="indented-line">${Fex_} = &pi;<sup>2</sup> ${E_} / (${Lcx_} / ${rx_})<sup>2</sup> = ${Fex.toFixed(2)} ksi</div>`;
+
+  const Fey = Math.PI**2 * E / (Lcy / ry)**2;
+  html += `<div class="indented-line">${Fey_} = &pi;<sup>2</sup> ${E_} / (${Lcy_} / ${ry_})<sup>2</sup> = ${Fey.toFixed(2)} ksi</div>`;
+
+  const Fez = (Math.PI**2 * E * Cw / Lcz**2 + G * J) / (Ag * ro**2);
+  html += `<div class="indented-line">${Fez_} = (&pi;<sup>2</sup> ${E_} ${Cw_} / ${Lcz_}<sup>2</sup> + ${G_} ${J_}) / (${Ag_} ${ro_}<sup>2</sup>) = ${Fez.toFixed(2)} ksi</div>`;
+
+  const ex = t / 2;
+  const ey = t / 2;
+  html += `<div class="indented-line">Shear center</div>
+           <div class="indented-line" style="--indented-line-level: 2;">${ex_} = ${t_} / 2 = ${ex.toFixed(3)} in.</div>
+           <div class="indented-line" style="--indented-line-level: 2;">${ey_} = ${t_} / 2 = ${ey.toFixed(3)} in.</div>`;
+           
+  const xo = Math.abs(x - ex);
+  const yo = Math.abs(y - ey);
+  html += `<div class="indented-line">Coordinates of shear center with respect to centroid</div>
+           <div class="indented-line" style="--indented-line-level: 2;">${xo_} = |${x_} - ${ex_}| = ${xo.toFixed(3)} in.</div>
+           <div class="indented-line" style="--indented-line-level: 2;">${yo_} = |${y_} - ${ey_}| = ${yo.toFixed(3)} in.</div>`;
+
+  const calcTerm1 = (xo / ro)**2;
+  const calcTerm1_ = `(${xo_} / ${ro_})<sup>2</sup>`;
+  const calcTerm2 = (yo / ro)**2;
+  const calcTerm2_ = `(${yo_} / ${ro_})<sup>2</sup>`;
+
+  const coeff1 = 1 - calcTerm1 - calcTerm2;
+  const coeff1_ = `(1 - ${calcTerm1_} - ${calcTerm2_})`;
+  const coeff2 = Fey * calcTerm1 + Fex * calcTerm2 - Fex - Fey - Fez;
+  const coeff2_ = `(${Fey_} ${calcTerm1_} + ${Fex_} ${calcTerm2_} - ${Fex_} - ${Fey_} - ${Fez_})`;
+  const coeff3 = Fex * Fey + Fey * Fez + Fex * Fez;
+  const coeff3_ = `(${Fex_} ${Fey_} + ${Fey_} ${Fez_} + ${Fex_} ${Fez_})`;
+  const coeff4 = -1 * Fex * Fey * Fez;
+  const coeff4_ = `${Fex_} ${Fey_} ${Fez_}`;
+  html += `<div class="indented-line">Solve for the lowest root of the cubic equation</div>
+           <div class="indented-line" style="--indented-line-level: 2;">(${Fe_} - ${Fex_}) (${Fe_} - ${Fey_}) (${Fe_} - ${Fez_}) - ${Fe_}<sup>2</sup> (${Fe_} - ${Fey_}) (${xo_} / ${ro_})<sup>2</sup> - ${Fe_}<sup>2</sup> (${Fe_} - ${Fex_}) (${yo_} / ${ro_})<sup>2</sup> = 0</div>
+           <div class="indented-line" style="--indented-line-level: 2;">${coeff1_} ${Fe_}<sup>3</sup> + ${coeff2_} ${Fe_}<sup>2</sup> + ${coeff3_} ${Fe_} - ${coeff4_} = 0</div>
+           <div class="indented-line" style="--indented-line-level: 2;">(${coeff1.toFixed(2)}) ${Fe_}<sup>3</sup> + (${coeff2.toFixed(2)}) ${Fe_}<sup>2</sup> + (${coeff3.toFixed(2)}) ${Fe_} + (${coeff4.toFixed(2)}) = 0</div>`;
+
+  const Fes = cubicEquationSolver(coeff1, coeff2, coeff3, coeff4);
+  Fes.sort((a, b) => a - b);
+  const Fes_ = ['F<sub>e,1</sub>', 'F<sub>e,2</sub>', 'F<sub>e,3</sub>'];
+  for (let i = 0; i < Fes.length; i++) {
+    html += `<div class="indented-line" style="--indented-line-level: 3;">${Fes_[i]} = ${Fes[i].toFixed(2)} ksi</div>`;
+  }
+
+  Fe = Fes[0];
+  html += `<div class="indented-line" style="--indented-line-level: 2;">${Fe_} = ${Fe.toFixed(2)} ksi</div>`;
+
+  return [Fe, html];
+}
+
+// Cubic Equation Solver
+function cubicEquationSolver(a, b, c, d) {
+  // standard cubic form: ax^3 + bx^2 + cx + d = 0
+
+  // normalize coefficients
+  b /= a;
+  c /= a;
+  d /= a;
+
+  // depressed cubic form: x^3 + px + q = 0
+  const p = c - b**2 / 3;
+  const q = 2 * b**3 / 27 - b * c / 3 + d;
+
+  // calculate discriminant
+  const discriminant = q**2 / 4 + p**3 / 27;
+
+  let roots = [];
+  if (discriminant > 0) {
+      // 1 real root
+      const u = Math.cbrt(-q / 2 + Math.sqrt(discriminant));
+      const v = Math.cbrt(-q / 2 - Math.sqrt(discriminant));
+      const root1 = u + v - b / 3;
+      roots.push(root1);
+
+  } else if (discriminant === 0) {
+      // multiple real roots (2 or 3 roots)
+      const u = Math.cbrt(-q / 2);
+      roots.push(2 * u - b / 3);
+      roots.push(-u - b / 3);
+
+  } else {
+      // 3 distinct real roots
+      const r = Math.sqrt(-(p**3) / 27);
+      const phi = Math.acos(-q / (2 * Math.sqrt(-(p**3) / 27)));
+      const t = 2 * Math.cbrt(r);
+
+      const root1 = t * Math.cos(phi / 3) - b / 3;
+      const root2 = t * Math.cos((phi + 2 * Math.PI) / 3) - b / 3;
+      const root3 = t * Math.cos((phi + 4 * Math.PI) / 3) - b / 3;
+
+      roots.push(root1, root2, root3);
+  }
+
+  return roots;
 }
 
 // Critical Stress Calculator
@@ -666,3 +845,9 @@ const Fel_ = 'F<sub>el</sub>';
 
 const Ag_ = 'A<sub>g</sub>';
 const Ae_ = 'A<sub>e</sub>';
+
+const ex_ = 'e<sub>x</sub>';
+const ey_ = 'e<sub>y</sub>';
+
+const xo_ = 'x<sub>o</sub>';
+const yo_ = 'y<sub>o</sub>';
